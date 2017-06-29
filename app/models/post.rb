b7516eada8000ab4d891ec9242442246252a98ab
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-06-30 05:33:31 +0800

    增加about页面

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5}
   validates :body, presence: true
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
